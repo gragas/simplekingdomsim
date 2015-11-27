829afc9000fb9c5cae30f55fdce3c3c4fccdb10d
Thomas David Fischer <tdf.tomfischer@gmail.com>
2015-11-26 18:02:31 -0600

    Wrote groudwork for _map.py (including my own original procedural generation algorithm!).

---
+++ _map.py
@@ -0,0 +1,153 @@
+import functools
+import random
+import sys
+import pygame
+from buffalo import utils
+from buffalo.scene import Scene
+
+class Map:
+
+    TILE_SIZE = 32
+
+    def __init__(self, size=(60, 34),
+                 i_temp=70, max_d_temp=5, off_d_temp=0,
+                 i_hum=30, max_d_hum=5, off_d_hum=0,
+                 i_alt=100, max_d_alt=5, off_d_alt=0,):
+        # size is a 2-tuple representing the width
+        # and height of the map in tiles
+        self.size = self.width, self.height = size
+        # i_* is the initial value of attribute *
+        # max_d_* is the maximum delta of attribute *
+        self.i_temp = i_temp
+        self.max_d_temp = max_d_temp
+        self.off_d_temp = off_d_temp
+        self.temp_arr = functools.reduce(
+            lambda x, y: x + y,
+            [[i + self.off_d_temp]*(self.max_d_temp - i) for i in range(self.max_d_temp)]
+        ) # e.g., [0, 0, 0, 0, 1, 1, 1, 2, 2, 3]
+        self.temp_arr = self.temp_arr + [-i for i in self.temp_arr]
+        self.i_hum = i_hum
+        self.max_d_hum = max_d_hum
+        self.off_d_hum = off_d_hum
+        self.hum_arr = functools.reduce(
+            lambda x, y: x + y,
+            [[i + self.off_d_hum]*(self.max_d_hum - i) for i in range(self.max_d_hum)]
+        ) # e.g., [0, 0, 0, 0, 1, 1, 1, 2, 2, 3]
+        self.hum_arr = self.hum_arr + [-i for i in self.hum_arr]
+        self.i_alt = i_alt
+        self.max_d_alt = max_d_alt
+        self.off_d_alt = off_d_alt
+        self.alt_arr = functools.reduce(
+            lambda x, y: x + y,
+            [[i + self.off_d_alt]*(self.max_d_alt - i) for i in range(self.max_d_alt)]
+        ) # e.g., [0, 0, 0, 0, 1, 1, 1, 2, 2, 3]
+        self.alt_arr = self.alt_arr + [-i for i in self.alt_arr]
+        self.tiles = dict()
+        self.surface = utils.empty_surface((self.width * Map.TILE_SIZE, self.height * Map.TILE_SIZE))
+        self.generate_map()
+        self.render()
+
+    def generate_map(self):
+        self.tiles[(0, 0)] = (self.i_temp, self.i_hum, self.i_alt)
+        edge_tiles = [(0, 0)]
+        while len(self.tiles) < (self.width * self.height):
+            while edge_tiles:
+                (x, y) = random.choice(edge_tiles)
+                for y_offset in range(-1, 2):
+                    for x_offset in range(-1, 2):
+                        x_cand, y_cand = x + x_offset, y + y_offset
+                        if ((x_cand, y_cand) not in self.tiles) and \
+                           (x_cand >= 0 and x_cand < self.width) and \
+                           (y_cand >= 0 and y_cand < self.height):
+                           edge_tiles.append((x_cand, y_cand))
+                           temp, hum, alt = self.tiles[(x, y)]
+                           self.tiles[(x_cand, y_cand)] = (
+                               temp + random.choice(self.temp_arr),
+                               hum + random.choice(self.hum_arr),
+                               alt + random.choice(self.alt_arr),
+                           )
+                edge_tiles.remove((x, y))
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                temp, hum, alt = self.tiles[(x, y)]
+                tmod = int(temp / 70.0 * 150)
+                r = tmod
+                g = tmod + int(abs(temp - 70) / 30.0 * 200)
+                b = tmod + int(hum / 100.0 * 200)
+                if r > 255: r = 255
+                if r < 0: r = 0
+                if g > 255: g = 255
+                if g < 0: g = 0
+                if b > 255: b = 255
+                if b < 0: b = 0
+                color = (r, g, b, 255)
+                self.surface.fill(
+                    color,
+                    pygame.Rect(
+                        x * Map.TILE_SIZE, y * Map.TILE_SIZE,
+                        Map.TILE_SIZE, Map.TILE_SIZE
+                    )
+                )
+
+    def blit(self, dest, pos=(0, 0)):
+        dest.blit(self.surface, pos)
+
+    def __str__(self):
+        max_tile = max(
+            self.tiles,
+            key=lambda j: len(str(max(self.tiles.get(j), key=lambda x: len(str(x)))))
+        )
+        num_chars = len(str(max(self.tiles[max_tile], key=lambda x: len(str(x)))))
+        temp_str = list()
+        hum_str = list()
+        alt_str = list()
+        for y in range(self.height):
+            temp_row = list()
+            hum_row = list()
+            alt_row = list()
+            for x in range(self.width):
+                temp, hum, alt = self.tiles[(x, y)]
+                temp_row.append(" "*(num_chars - len(str(temp))) + str(temp))
+                hum_row.append(" "*(num_chars - len(str(hum))) + str(hum))
+                alt_row.append(" "*(num_chars - len(str(alt))) + str(alt))
+            temp_str.append(" ".join(temp_row))
+            hum_str.append(" ".join(hum_row))
+            alt_str.append(" ".join(alt_row))
+        temp_str = "Temperatures:\n" + "\n".join(temp_str)
+        hum_str = "Humdities:\n" + "\n".join(hum_str)
+        alt_str = "Altitudes:\n" + "\n".join(alt_str)
+        return "\n\n".join([temp_str, hum_str, alt_str])
+
+class _MainScene(Scene):
+    # This class is only used when __name__ == "__main__"
+    # It displays a default map
+    def __init__(self):
+        Scene.__init__(self)
+        self.m = Map()
+
+    def on_escape(self):
+        sys.exit()
+
+    def blit(self):
+        self.m.blit(utils.screen)
+
+    def update(self):
+        pass
+
+def main():
+    while not utils.end:
+        utils.scene.logic()
+        utils.scene.update()
+        utils.scene.render()
+        utils.delta = utils.clock.tick( utils.FRAMES_PER_SECOND )    
+
+if __name__ == "__main__":
+    if not utils.init(caption="Simple Kingdom Simulator", fullscreen=True,):
+        print("Buffalo failed to initialize")
+        pygame.quit()
+        sys.exit()
+    utils.set_scene(_MainScene())
+    main()
+    pygame.quit()
